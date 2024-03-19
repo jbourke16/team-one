aa9afcd1152e77cd90044f0d0a8683e64f69e97d
Grace Clower <clower.grace@gmail.com>
2024-03-19 12:20:43 -0400

    created sign in form and functions

--- src/screens/AuthForms/SignIn.jsx
+++ src/screens/AuthForms/SignIn.jsx
@@ -1,9 +1,85 @@
 import React from 'react'
 import './AuthForm.css'
+import {useState} from 'react'
+import { signIn } from '../../services/users.js'
+import { useNavigate } from 'react-router-dom'
+import userEvent from '@testing-library/user-event'
 
-function SignIn() {
+function SignIn(props) {
+  const navigate = useNavigate()
+
+  const [form, setForm] = useState ({
+    email: '',
+    password: '',
+    isError: false,
+    errorMsg: '',
+  })
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const onSignIn = async (e) => {
+    e.preventDefault()
+    const {setUser} = props
+    try {
+      const user = await signIn(form)
+      setUser(user)
+      navigate('/games')
+    } catch (error) {
+      console.error(error)
+      setForm({
+        isError: true,
+        errorMsg: 'Invalid Credentials',
+        email: '',
+        password: '',
+      })
+    }
+  }
+
+  const renderError = () => {
+    const toggleForm = form.isError ? 'danger' : ''
+    if(form.isError) {
+      return(
+        <button type='submit' className={toggleForm}>
+          {form.errorMsg}
+        </button>
+      )
+    } else {
+      return <button type='submit'>Sign In</button>
+    }
+  }
+
+  const {email, password } = form
+
   return (
-    <div>SignIn</div>
+    <div className='form-container'>
+      <h3>Sign In</h3>
+      <form onSubmit={onSignIn}>
+        <label>Email</label>
+        <input
+          required
+          type='text'
+          name='email'
+          value={email}
+          placeholder='Enter Email'
+          onChange={handleChange}
+        />
+        <label>Password</label>
+        <input
+          required
+          name='password'
+          value={password}
+          type='password'
+          placeholder='Password'
+          onChange={handleChange}
+        />
+        {renderError()}
+      </form>
+    </div>
   )
 }
 
