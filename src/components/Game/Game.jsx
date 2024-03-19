d9af8cfa96f03ebb9f5782bef28cea55a0ff0f99
Jenn Bourke <jennfbourke@gmail.com>
2024-03-19 15:21:31 -0400

    Added games to Game screen (display and resize images), fixed sign in view when looking at the games page

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './Game.css'
 
-function Game() {
+function Game (props) {
   return (
-    <div>Game</div>
+    <div>
+      <img className="game-image" src={props.image} alt={props.name}/>
+
+      <div className="game-name">{props.name}</div>
+    </div>
   )
 }
 
