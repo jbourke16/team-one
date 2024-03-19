8015fb40884e8c2a16a1a08b307df77e6cea4a2c
arehmanlatif1 <rehmanlatif335@gmail.com>
2024-03-19 12:04:24 -0400

    make a link with nav to home

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Home.css'
+import Nav from "../../components/Nav/Nav.jsx";
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Nav />
+    </div>
   )
 }
 
