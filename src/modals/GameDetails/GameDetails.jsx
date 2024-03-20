845c46f58c3f8d8e66e4f575ffc1eeacc7716299
arehmanlatif1 <rehmanlatif335@gmail.com>
2024-03-20 14:19:02 -0400

    game details pop up works

--- src/modals/GameDetails/GameDetails.jsx
+++ src/modals/GameDetails/GameDetails.jsx
@@ -1,30 +1,30 @@
-import React from "react"
-import { useState, useEffect } from "react";
-import { Link, useParams, useNavigate } from "react-router-dom"
-import "./GameDetails.css"
-import { getGame } from "../../services/games.js"
+// import React from "react"
+// import { useState, useEffect } from "react";
+// import { Link, useParams, useNavigate } from "react-router-dom"
+// import "./GameDetails.css"
+// import { getGame } from "../../services/games.js"
 
-export default function GameDetails() {
-  const [game, setGame] = useState(false);
+// export default function GameDetails() {
+//   const [game, setGame] = useState(false);
 
-  let { id } = useParams();
-  let navigate = useNavigate();
+//   let { id } = useParams();
+//   let navigate = useNavigate();
 
-  const fetch = async () => {
-    const oneGame = await getGame(id);
-    setGame(oneGame);
-  }
+//   const fetch = async () => {
+//     const oneGame = await getGame(id);
+//     setGame(oneGame);
+//   }
 
 
-  const toggleGameDetails = () => {
-    setGame(!game);
-  };
+//   const toggleGameDetails = () => {
+//     setGame(!game);
+//   };
 
-  return (
-    <div>
-      <h1> welcome to {game.name}</h1>
-      <img src={game.img} alt={game.id} />
-    </div>
+//   return (
+//     <div>
+//       <h1> welcome to {game.name}</h1>
+//       <img src={game.img} alt={game.id} />
+//     </div>
 
-  );
-}
+//   );
+// }
